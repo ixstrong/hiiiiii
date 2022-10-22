f19c26aa83c36c79eacb2bbdd626adf4eaed42a5
ixstrong <60701332+ixstrong@users.noreply.github.com>
2022-10-22 06:42:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-
-client.on("ready", () => {
-  const channel = client.channels.get("705062650073317407");
-  if (!channel) return console.error("The channel does not exist!");
-  channel.join().then(connection => {
-    console.log("Successfully connected.");
-  }).catch(e => {
-    console.error(e);
+client.on('message', msg => {
+    if(msg.content === '$help')
+    msg.reply('Check Your DM :white_check_mark:')
   });
-});
-
-
 
 client.login(process.env.TOKEN);
